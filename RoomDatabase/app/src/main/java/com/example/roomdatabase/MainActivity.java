d13f8010c874cd4568e0d7598c1223d169e08ad7
Tr Th Minh Tân <mita37688>
2021-05-12 16:40:10 +0700

    Basic Room

---
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "RoomDemo").build();
+
+        UserDao userDao = db.userDao();
+        List<User> users = userDao.getAll();
+    }
+}
---
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/UserDao.java
@@ -0,0 +1,27 @@
+package com.example.roomdatabase;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM User")
+    List<User> getAll();
+
+    @Query("SELECT * FROM User WHERE id IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM User WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
